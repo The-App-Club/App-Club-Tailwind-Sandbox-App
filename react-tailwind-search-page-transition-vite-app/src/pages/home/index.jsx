0d969e5ec392ab77a53bf7cb64be31e64da3bc4e
Higashi-Kota <higashi.kota@gri.jp>
2022-08-17 01:34:58 +0900

    fix tabindex button

--- react-tailwind-search-page-transition-vite-app/src/pages/home/index.jsx
+++ react-tailwind-search-page-transition-vite-app/src/pages/home/index.jsx
@@ -308,8 +308,9 @@ const HomePage = ({pageName, notifier}) => {
               />
               <button
                 type="submit"
-                className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+                className="text-white absolute right-2.5 bottom-2.5 bg-gray-700 font-medium rounded-lg text-sm px-4 py-2"
                 onClick={(e) => {}}
+                disabled
               >
                 Esc
               </button>
