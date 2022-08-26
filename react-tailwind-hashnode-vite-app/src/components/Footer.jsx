c8fe9cfc5a159cdd5f4728db2d24ac9253a0b04f
Higashi-Kota <higashi.kota@gri.jp>
2022-08-26 14:09:11 +0900

    update footer

--- react-tailwind-hashnode-vite-app/src/components/Footer.jsx
+++ react-tailwind-hashnode-vite-app/src/components/Footer.jsx
@@ -1,16 +1,51 @@
 import {css, cx} from '@emotion/css';
+import {Link} from 'react-router-dom';
+
+import {BsTwitter} from 'react-icons/bs';
+import {FaFacebookF} from 'react-icons/fa';
+import {AiOutlineCopyright} from 'react-icons/ai';
+import {AiFillInstagram} from 'react-icons/ai';
+import {BsPinterest} from 'react-icons/bs';
+
+import logo from '../assets/logo.png';
 
 const Footer = () => {
   return (
     <footer
       className={cx(
         css``,
-        `text-xl flex justify-center items-center`,
-        `border-t-2`,
-        `pt-2 pb-2`
+        `text-xl flex justify-center flex-col items-center`,
+        `border-t-2 py-2`
       )}
     >
-      Bye
+      <Link to={'/'} className={`flex items-center gap-2`}>
+        <img src={logo} alt={'logo'} className={`w-10`} />
+        <h2 className="text-xl">Make YourSelf</h2>
+      </Link>
+      <div className="flex flex-col items-center gap-1">
+        <div className="flex items-center gap-1">
+          <BsTwitter
+            size={24}
+            className={'hover:cursor-pointer hover:text-blue-400'}
+          />
+          <FaFacebookF
+            size={24}
+            className={'hover:cursor-pointer hover:text-blue-900'}
+          />
+          <AiFillInstagram
+            size={24}
+            className={'hover:cursor-pointer hover:text-pink-700'}
+          />
+          <BsPinterest
+            size={24}
+            className={'hover:cursor-pointer hover:text-red-700'}
+          />
+        </div>
+        <div className="flex items-center">
+          <AiOutlineCopyright size={16} />
+          <span className="text-sm">Make YourSelf</span>
+        </div>
+      </div>
     </footer>
   );
 };
--- react-tailwind-hashnode-vite-app/src/components/MainFeeder..jsx
+++ react-tailwind-hashnode-vite-app/src/components/MainFeeder..jsx
@@ -2,7 +2,7 @@ import {MainFeed} from './MainFeed';
 
 const MainFeeder = ({fetchType}) => {
   return (
-    <div className="flex flex-col w-full  gap-4 justify-center pt-2 pb-2">
+    <div className="flex flex-col w-full gap-4 justify-center py-2">
       <MainFeed feedType={fetchType} />
       {/* <MainFeed feedType={fetchType} />
       <MainFeed feedType={fetchType} />
