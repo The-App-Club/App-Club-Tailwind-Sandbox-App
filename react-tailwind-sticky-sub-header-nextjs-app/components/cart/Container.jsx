0200814e0312647f81890f021cc7a569ab96e8cd
Higashi-Kota <higashi.kota@gri.jp>
2022-10-13 01:00:43 +0900

    optimaized rendering on cart page

---
+++ react-tailwind-sticky-sub-header-nextjs-app/components/cart/Container.jsx
@@ -0,0 +1,54 @@
+import {css, cx} from '@emotion/css';
+import Link from 'next/link';
+import {memo, useEffect, useState} from 'react';
+import useCart from '../../hooks/useCart';
+import ProductCartItem from '../ProductCartItem';
+import ReviewRanking from '../ReviewRanking';
+
+const Container = () => {
+  const [isClient, setIsClient] = useState(false);
+  const {carts} = useCart();
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsClient(true);
+    }
+  }, []);
+  const renderCartContent = () => {
+    if (carts.length === 0) {
+      return (
+        <div
+          className={cx(
+            `w-full flex justify-center flex-col items-center`,
+            `border-2  rounded-lg shadow-lg p-2`
+          )}
+        >
+          <p>Nothing wines in cart...</p>
+          <Link href={`/wines`}>
+            <a className="hover:underline">See Wines</a>
+          </Link>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className={css`
+            width: 100%;
+            display: flex;
+            justify-content: flex-start;
+            flex-direction: column;
+            align-items: flex-start;
+            gap: 3rem;
+          `}
+        >
+          {carts.map((item, index) => {
+            return <ProductCartItem key={index} item={item} />;
+          })}
+        </div>
+      );
+    }
+  };
+
+  return <>{isClient && renderCartContent()}</>;
+};
+
+export default memo(Container);
--- react-tailwind-sticky-sub-header-nextjs-app/pages/cart.js
+++ react-tailwind-sticky-sub-header-nextjs-app/pages/cart.js
@@ -1,75 +1,17 @@
 import {css, cx} from '@emotion/css';
-import Link from 'next/link';
 import Sidebar from '../components/Sidebar';
 import Layout from '../layouts/default';
 import hamburgerState from '../stores/hamburgerStore';
-import {useRecoilState, useRecoilValue} from 'recoil';
+import {useRecoilValue} from 'recoil';
 import Breadcrumbs from 'nextjs-breadcrumbs';
 import capitalize from 'capitalize-the-first-letter';
-import cartState from '../stores/cartStore';
-import ProductGalleryItem from '../components/ProductGalleryItem';
-import useCart from '../hooks/useCart';
-import {useEffect, useState} from 'react';
 import Spacer from '../components/Spacer';
-import ProductCartItem from '../components/ProductCartItem';
 import ReviewRanking from '../components/ReviewRanking';
-import FlipMove from 'react-flip-move';
-import CartSummary from '../components/CartSummary';
+import Header from '../components/cart/Header';
+import Container from '../components/cart/Container';
 
 const Cart = () => {
-  const [isClient, setIsClient] = useState(false);
   const {opened} = useRecoilValue(hamburgerState);
-  const {carts, removeAllFromCart} = useCart();
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setIsClient(true);
-    }
-  }, []);
-
-  const renderCartContent = () => {
-    if (carts.length === 0) {
-      return (
-        <div
-          className={cx(
-            `w-full flex justify-center flex-col items-center`,
-            `border-2  rounded-lg shadow-lg p-2`
-          )}
-        >
-          <p>Nothing wines in cart...</p>
-          <Link href={`/wines`}>
-            <a className="hover:underline">See Wines</a>
-          </Link>
-        </div>
-      );
-    } else {
-      return (
-        <div
-          className={css`
-            width: 100%;
-            display: flex;
-            justify-content: flex-start;
-            flex-direction: column;
-            align-items: flex-start;
-            gap: 3rem;
-          `}
-        >
-          {carts.map((item, index) => {
-            return <ProductCartItem key={index} item={item} />;
-          })}
-        </div>
-      );
-    }
-  };
-
-  const handleRemoveAllFromCart = (e) => {
-    removeAllFromCart();
-  };
-
-  const handleCheckout = (e) => {
-    console.log(`checkout`);
-  };
-
   return (
     <>
       <Sidebar />
@@ -123,56 +65,7 @@ const Cart = () => {
               return `${niceTitle} > `;
             }}
           />
-          <div
-            className={cx(
-              css`
-                z-index: 3;
-                position: sticky;
-                top: 6rem;
-                min-height: 3rem;
-                width: 100%;
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                flex-direction: column;
-                gap: 1rem;
-              `,
-              `bg-white dark:bg-slate-700 shadow-md p-2`
-            )}
-          >
-            <div className="w-full flex justify-between items-center gap-4">
-              <h2
-                className={cx(
-                  `w-full text-xl flex flex-col justify-start gap-1`,
-                  css`
-                    @media (max-width: 768px) {
-                      flex-direction: column;
-                      align-items: flex-start;
-                    }
-                  `
-                )}
-              >
-                Cart
-              </h2>
-              <div className="flex items-center gap-2">
-                <button
-                  className="px-2 py-2 bg-blue-500 hover:bg-blue-800 text-white rounded-lg w-24 text-sm text-center"
-                  onClick={handleRemoveAllFromCart}
-                >
-                  Remove All
-                </button>
-              </div>
-              <div className="flex items-center gap-2">
-                <button
-                  className="px-2 py-2 bg-blue-500 hover:bg-blue-800 text-white rounded-lg w-24 text-sm text-center"
-                  onClick={handleCheckout}
-                >
-                  Checkout
-                </button>
-              </div>
-            </div>
-            <CartSummary />
-          </div>
+          <Header />
           <Spacer />
           <div
             className={cx(
@@ -191,7 +84,7 @@ const Cart = () => {
               `
             )}
           >
-            {isClient && renderCartContent()}
+            <Container />
             <ReviewRanking
               className={css`
                 position: sticky;
