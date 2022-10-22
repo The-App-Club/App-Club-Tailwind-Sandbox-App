a2c62eaf1fa39d839d073b44103785ba63930282
Higashi-Kota <higashi.kota@gri.jp>
2022-10-22 16:16:24 +0900

    add sample

--- react-custom-color-mergician-pallete-vite-app/tailwind.config.js
+++ react-custom-color-mergician-pallete-vite-app/tailwind.config.js
@@ -9,17 +9,27 @@ module.exports = {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
       },
-    },
-    colors: mergician(colors, {
-      bebop: {
-        // https://tailwindcss.com/docs/theme#colors
-        // https://colorhunt.co/palettes/night
-        100: '#001E6C',
-        200: '#035397',
-        300: '#5089C6',
-        400: '#FFAA4C',
+      colors: {
+        bebop: {
+          // https://tailwindcss.com/docs/theme#colors
+          // https://colorhunt.co/palettes/night
+          100: '#001E6C',
+          200: '#035397',
+          300: '#5089C6',
+          400: '#FFAA4C',
+        },
       },
-    }),
+    },
+    // colors: mergician(colors, {
+    //   bebop: {
+    //     // https://tailwindcss.com/docs/theme#colors
+    //     // https://colorhunt.co/palettes/night
+    //     100: '#001E6C',
+    //     200: '#035397',
+    //     300: '#5089C6',
+    //     400: '#FFAA4C',
+    //   },
+    // }),
   },
   plugins: [
     require('tailwindcss-no-scrollbar'),
