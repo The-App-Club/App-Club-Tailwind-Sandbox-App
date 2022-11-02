b29e7e989511464f8820556805c500a8037fce66
Higashi-Kota <higashi.kota@gri.jp>
2022-11-02 11:23:20 +0900

    fix dependencies

--- react-tailwindcss-fullcalender-nextjs-app/pages/api/listUpWines.js
+++ react-tailwindcss-fullcalender-nextjs-app/pages/api/listUpWines.js
@@ -1,7 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import data from '../../data/wines.json';
-
 export default function handler(req, res) {
-  res.status(200).json({data});
+  res.status(200).json({message: 'hello'});
 }
